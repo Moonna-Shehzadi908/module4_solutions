8b5b6d8ba558d17503e01e30c897e19e643cbf77
Moonna-Shehzadi908 <22021519-030@uog.edu.pk>
2025-07-13 01:41:30 +0500

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,10 +1,8 @@
+// SpeakHello.js
 (function (window) {
-  var speakWord = "Hello";
-
   var helloSpeaker = {};
-
   helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
+    console.log("Hello " + name);
   };
 
   window.helloSpeaker = helloSpeaker;
